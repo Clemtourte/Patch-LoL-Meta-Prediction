c3c7c3b7b62df14fca41b73f10bc1bcaeaafd35e
Clement Tourte <yanquille92@gmail.com>
2024-05-13 20:14:43 +0200

    feat: Add method to retrieve match information

--- src/main.py
+++ src/main.py
@@ -15,6 +15,28 @@ class User:
     def get_matches(self, match_type, match_count):
         response = requests.get(f"https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{self.puuid}/ids?type={match_type}&start=0&count={match_count}&api_key={self.API_key}")
         return response.json()
+    
+    def get_match_data(self,match_id):
+        response = requests.get(f"https://europe.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={self.API_key}")
+        return response.json()
+    
+    def get_match_info(self, match_id):
+        match_data = self.get_match_data(match_id)
+        game_id = match_data['metadata']['matchId']
+        game_duration = match_data['info']['gameDuration']
+        game_version = match_data['info']['gameVersion']
+        participants = match_data['info']['participants']
+        teams = match_data['info']['teams']
+        timeline = match_data['info']['timeline']
+
+        return {
+            'game_id': game_id,
+            'game_duration':game_duration,
+            'game_version': game_version,
+            'participants': participants,
+            'teams': teams,
+            'timeline': timeline,
+        }
 
 user = User('MenuMaxiBestFlop','EUW')
 print(user.puuid)
@@ -23,3 +45,7 @@ match_type = "ranked"  # replace with the actual match type
 match_count = 50  # replace with the actual match count
 match_id_list = user.get_matches(match_type, match_count)
 print(match_id_list)
+
+if match_id_list:
+    match_data = user.get_match_data(match_id_list[0])
+    print(match_data)
