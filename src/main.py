61e9339b7348d22b00cce76ac311ec49305ee173
Clement Tourte <yanquille92@gmail.com>
2024-06-14 22:35:08 +0200

    back to main.py file to simplify the thought process

--- src/main.py
+++ src/main.py
@@ -1,44 +1,148 @@
-import sqlite3
-from user import User
+import requests
+import os
+from dotenv import load_dotenv
+import logging
 
-def create_connection(db_file):
-    """ Create a database connection to the SQLite database specified by db_file """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print("SQLite version:", sqlite3.version)
-    except sqlite3.Error as e:
-        print(e)
-    return conn
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-def create_table(conn):
-    """ Create a table for storing user information """
-    try:
-        sql_create_user_table = """ CREATE TABLE IF NOT EXISTS users (
-                                        id integer PRIMARY KEY,
-                                        username text NOT NULL,
-                                        tag text NOT NULL,
-                                        region text NOT NULL,
-                                        puuid text NOT NULL,
-                                        tier text,
-                                        rank text,
-                                        league_points integer
-                                    ); """
-        c = conn.cursor()
-        c.execute(sql_create_user_table)
-        print("User table created")
-    except sqlite3.Error as e:
-        print(e)
+class User:
+    def __init__(self, username, tag, region):
+        load_dotenv()  # Load environment variables from .env file
+        self.API_key = os.getenv('API_KEY')
+        if not self.API_key:
+            logging.error("API_KEY not found in .env file")
+        else:
+            logging.info(f"API_KEY loaded successfully: {self.API_key}")
+        self.username = username
+        self.tag = tag
+        self.region = region
+        self.puuid = self.get_puuid()
+        print(f"Username: {username}")
 
-# Main execution
-database = "user_data.db"
-conn = create_connection(database)
-if conn:
-    create_table(conn)
+    def get_puuid(self):
+        url = f"https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{self.username}/{self.tag}?api_key={self.API_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            try:
+                return response.json()['puuid']
+            except KeyError:
+                logging.error("Key 'puuid' not found in response. Response: %s", response.json())
+        else:
+            logging.error("API request failed with status code %s. Response: %s", response.status_code, response.json())
+        return None
+    
+    def get_summoner_id(self):
+        url = f"https://{self.region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{self.puuid}?api_key={self.API_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            try:
+                return response.json()['id']
+            except KeyError:
+                logging.error("Key 'id' not found in response. Response: %s", response.json())
+        else:
+            logging.error("API request failed with status code %s. Response: %s", response.status_code, response.json())
+        return None
 
-    # Create a User object and store its information in the database
+    def get_ranked_data(self, summoner_id):
+        url = f"https://{self.region}.api.riotgames.com/lol/league/v4/entries/by-summoner/{summoner_id}?api_key={self.API_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            try:
+                return response.json()
+            except Exception as e:
+                logging.error("Error parsing response: %s. Response: %s", e, response.text)
+        else:
+            logging.error("API request failed with status code %s. Response: %s", response.status_code, response.json())
+        return []
+
+    def display_user_info(self):
+        summoner_id = self.get_summoner_id()
+        if not summoner_id:
+            logging.error("Failed to get summoner ID.")
+            return
+
+        ranked_data = self.get_ranked_data(summoner_id)
+        for entry in ranked_data:
+            if entry.get('queueType') == 'RANKED_SOLO_5x5':
+                tier = entry['tier']
+                rank = entry['rank']
+                league_points = entry['leaguePoints']
+                print(f"Tier: {tier} {rank}, League Points: {league_points}")
+                return tier, rank, league_points
+        print("No ranked data found for the summoner.")
+
+    def get_matches(self, match_type, match_count):
+        url = f"https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{self.puuid}/ids?type={match_type}&start=0&count={match_count}&api_key={self.API_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            logging.error("API request failed with status code %s. Response: %s", response.status_code, response.json())
+        return []
+
+    def get_match_data(self, match_id):
+        url = f"https://europe.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={self.API_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            logging.error("API request failed with status code %s. Response: %s", response.status_code, response.json())
+        return {}
+
+    def get_match_info(self, match_id):
+        match_data = self.get_match_data(match_id)
+        if not match_data:
+            return {}
+
+        try:
+            game_id = match_data['metadata']['matchId']
+            game_duration = match_data['info']['gameDuration']
+            game_version = match_data['info']['gameVersion']
+
+            general_info = {
+                'game_id': game_id,
+                'game_duration': round(game_duration / 60, 2),
+                'patch': game_version.split('.')
+            }
+
+            participant_info = {'Blue Side': {}, 'Red Side': {}}
+            for i, participant in enumerate(match_data['info']['participants'], 1):
+                team = 'Blue Side' if participant['teamId'] == 100 else 'Red Side'
+                participant_info[team][f'Summoner {i}'] = {
+                    'summoner_id': participant['puuid'],
+                    'summoner_name': participant['summonerName'],
+                    'team': participant['teamId'],
+                    'champ_level': participant['champLevel'],
+                    'champ_name': participant['championName'],
+                    'champ_id': participant['championId'],
+                    'kills': participant['kills'],
+                    'deaths': participant['deaths'],
+                    'assists': participant['assists'],
+                }
+
+            return general_info, participant_info
+
+        except KeyError as e:
+            logging.error("Key error: %s. Here's the entire response: %s", e, match_data)
+            return {}
+
+# Example usage
+if __name__ == '__main__':
     user = User('MenuMaxiBestFlop', 'EUW', 'EUW1')
-    user.store_user_info(conn)
-
-    # Close the connection
-    conn.close()
+    user.display_user_info()
+    
+    match_ids = user.get_matches('ranked', 1)
+    for match_id in match_ids:
+        general_info, participant_info = user.get_match_info(match_id)
+        if general_info:
+            print(f"Match ID: {general_info['game_id']}")
+            print(f"Game Duration: {general_info['game_duration']} minutes")
+            print(f"Patch: {'.'.join(general_info['patch'])}")
+            print("Participants Info:")
+            for team, participants in participant_info.items():
+                print(team)
+                for summoner, details in participants.items():
+                    print(f"{summoner}: {details}")
+            print("\n")
+        else:
+            print(f"Failed to retrieve data for match ID: {match_id}")
