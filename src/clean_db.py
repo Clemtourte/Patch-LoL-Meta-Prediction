aca1239db0408e47b577bba76a621069930de15c
Clément Tourte <yanquille92@gmail.com>
2025-03-06 20:41:34 +0100

    empty patch info tables

--- src/clean_db.py
+++ src/clean_db.py
@@ -1,30 +1,32 @@
+#!/usr/bin/env python
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import SpellStats, ChampionStats, ItemStats
+from models import SpellStats, ChampionStats, ItemStats, PatchChanges
 
-def clean_tables():
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def empty_tables():
     engine = create_engine('sqlite:///../datasets/league_data.db')
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     try:
-        print("Deleting records from SpellStats...")
-        session.query(SpellStats).delete()
-        
-        print("Deleting records from ChampionStats...")
-        session.query(ChampionStats).delete()
-        
-        print("Deleting records from ItemStats...")
-        session.query(ItemStats).delete()
-        
+        deleted_spellstats = session.query(SpellStats).delete()
+        deleted_championstats = session.query(ChampionStats).delete()
+        deleted_itemstats = session.query(ItemStats).delete()
+        deleted_patchchanges = session.query(PatchChanges).delete()
         session.commit()
-        print("All tables cleaned successfully!")
-        
+        logger.info(f"Emptied SpellStats table. Deleted {deleted_spellstats} rows.")
+        logger.info(f"Emptied ChampionStats table. Deleted {deleted_championstats} rows.")
+        logger.info(f"Emptied ItemStats table. Deleted {deleted_itemstats} rows.")
+        logger.info(f"Emptied PatchChanges table. Deleted {deleted_patchchanges} rows.")
     except Exception as e:
-        print(f"Error while cleaning tables: {e}")
+        logger.error(f"Error emptying tables: {e}")
         session.rollback()
     finally:
         session.close()
 
 if __name__ == "__main__":
-    clean_tables()
+    empty_tables()
